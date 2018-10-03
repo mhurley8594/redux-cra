4338e664cfd763aa7d406154ca9c77c0fd61c077
Michael Hurley <mike@swampsoftware.org>
2018-10-03 13:15:29 -0400

    Added example actions, reducers, and store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import SomeComponent from './components/SomeComponent/SomeComponent';
+import './styles/styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <Provider store={store}>
+      <SomeComponent />
+  </Provider>,
+  document.getElementById('app')
+);
